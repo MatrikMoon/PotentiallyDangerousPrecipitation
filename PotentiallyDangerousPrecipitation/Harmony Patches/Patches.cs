68def46729d128be7f2851ca8409bb39a88c4f5c
MatrikMoon <5924350+MatrikMoon@users.noreply.github.com>
2021-04-18 15:55:33 -0500

    Initial commit
    Updated to remove ryder and replace with Harmony

---
+++ PotentiallyDangerousPrecipitation/Harmony Patches/Patches.cs
@@ -0,0 +1,159 @@
+﻿using HarmonyLib;
+using RoR2;
+using RoR2.Artifacts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/**
+ * Patching method taken appreciatively from Beat Saber Multiplayer on 10/15/2020 by Moon
+ * Other methods crafted for Cross Platform purposes
+ */
+
+namespace PotentiallyDangerousPrecipitation.HarmonyPatches
+{
+    public static class Patches
+    {
+        public static void Patch()
+        {
+            var instance = new Harmony(typeof(Patches).FullName);
+
+            Logger.Debug("Patching...");
+            foreach (var type in Assembly.GetExecutingAssembly()
+                            .GetTypes()
+                            .Where(x => x.IsClass && x.Namespace == typeof(Patches).Namespace))
+            {
+                List<MethodInfo> patchedMethods = instance.CreateClassProcessor(type).Patch();
+                if (patchedMethods != null && patchedMethods.Count > 0)
+                {
+                    foreach (var method in patchedMethods)
+                    {
+                        Logger.Debug($"Patched {method.DeclaringType}.{method.Name}!");
+                    }
+                }
+            }
+            Logger.Info("Applied patches!");
+        }
+    }
+
+    [HarmonyPatch(typeof(GenericPickupController))]
+    public class GenericPickupControllerPatch
+    {
+        [HarmonyPatch("GrantItem")]
+        [HarmonyPatch(new[] { typeof(CharacterBody), typeof(Inventory) })]
+        static bool Prefix(GenericPickupController __instance, CharacterBody body, Inventory inventory)
+        {
+            if (!NetworkServer.active)
+            {
+                Debug.LogWarning("[Server] function 'System.Void RoR2.GenericPickupController::GrantItem(RoR2.CharacterBody,RoR2.Inventory)' called on client");
+                return false;
+            }
+
+            PickupDef pickupDef = PickupCatalog.GetPickupDef(__instance.pickupIndex);
+
+            var count = Precipitation.HighStacks ? 10 : 1;
+            inventory.GiveItem((pickupDef != null) ? pickupDef.itemIndex : ItemIndex.None, count);
+
+            typeof(GenericPickupController).InvokeMethod("SendPickupMessage", inventory.GetComponent<CharacterMaster>(), __instance.pickupIndex);
+
+            UnityEngine.Object.Destroy(__instance.gameObject);
+
+            return false;
+        }
+
+        [HarmonyPatch(MethodType.Setter)]
+        [HarmonyPatch("NetworkRecycled")]
+        [HarmonyPatch(new[] { typeof(bool) })]
+        static bool Prefix(GenericPickupController __instance, bool value)
+        {
+            if (Precipitation.InfiniteRecycling) Logger.Debug("Prevented recycler from setting!");
+            return !Precipitation.InfiniteRecycling;
+        }
+    }
+
+    [HarmonyPatch(typeof(Util))]
+    public class UtilPatch
+    {
+        [HarmonyPatch("EscapeRichTextForTextMeshPro")]
+        [HarmonyPatch(new[] { typeof(string) })]
+        static bool Prefix(string rtString, ref string __result)
+        {
+            __result = "<noparse>" + rtString + "</noparse>";
+            return false;
+        }
+
+        [HarmonyPatch("GetExpAdjustedDropChancePercent")]
+        [HarmonyPatch(new[] { typeof(float), typeof(GameObject)})]
+        static bool Prefix(ref float __result)
+        {
+            if (Precipitation.PerfectLootChance)
+            {
+                __result = 100;
+                return false;
+            }
+            else return true;
+        }
+    }
+
+    [HarmonyPatch]
+    public class AntiLogRedirectPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            //Patch the thing that steals logs from me
+            var assembly = AppDomain.CurrentDomain.GetAssemblies().First(x => x.FullName.Contains("Assembly-CSharp"));
+            var redirectorType = assembly.GetType("RoR2.UnitySystemConsoleRedirector");
+            return redirectorType.GetMethod("Redirect", ReflectionUtil._allBindingFlags);
+        }
+
+        static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BasicPickupDropTable))]
+    public class BasicPickupDropTablePatch
+    {
+        [HarmonyPatch("GenerateDrop")]
+        [HarmonyPatch(new[] { typeof(Xoroshiro128Plus) })]
+        static bool Prefix(Xoroshiro128Plus rng, ref PickupIndex __result)
+        {
+            var original = typeof(SacrificeArtifactManager).GetField<PickupDropTable>("dropTable");
+            if (Precipitation.PerfectFuelCellChance)
+            {
+                __result = rng.NextElementUniform(new List<PickupIndex>() {
+                    PickupCatalog.FindPickupIndex("ItemIndex.EquipmentMagazine"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.AutoCastEquipment"),
+                });
+                return false;
+            }
+            else if (Precipitation.PerfectProcItemChance)
+            {
+                __result = rng.NextElementUniform(new List<PickupIndex>() {
+                    PickupCatalog.FindPickupIndex("ItemIndex.ChainLightning"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.Missile"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.Dagger"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.ShockNearby"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.BounceNearby"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.Icicle"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.LaserTurbine"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.NovaOnHeal"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.Thorns"),
+                    PickupCatalog.FindPickupIndex("ItemIndex.BurnNearby")
+                });
+                return false;
+            }
+            else if (Precipitation.PerfectLegendaryChance)
+            {
+                var selector = original.GetField<WeightedSelection<List<PickupIndex>>>("selector");
+                __result = rng.NextElementUniform(selector.choices[2].value);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ PotentiallyDangerousPrecipitation/Precipitation.cs
@@ -0,0 +1,174 @@
+﻿using IllusionPlugin;
+using PotentiallyDangerousPrecipitation.HarmonyPatches;
+using RoR2;
+using RoR2.UI;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace PotentiallyDangerousPrecipitation
+{
+    class Precipitation : IPlugin
+    {
+        public string Name => "PotentiallyDangerousPrecipitation";
+        public  string Version => "0.0.0";
+
+        public static bool PerfectLootChance { get; set; }
+        public static bool PerfectFuelCellChance { get; set; }
+        public static bool PerfectProcItemChance { get; set; }
+        public static bool PerfectLegendaryChance { get; set; }
+        public static bool HighStacks { get; set; }
+        public static bool InfiniteRecycling { get; set; }
+
+        public void OnApplicationQuit()
+        {
+
+        }
+
+        public void OnApplicationStart()
+        {
+            //View unity logs as well as my own
+            Application.logMessageReceivedThreaded += Application_logMessageReceivedThreaded;
+
+            //Hook the item grant functionality
+            Patches.Patch();
+        }
+
+        private void Application_logMessageReceivedThreaded(string condition, string stackTrace, LogType type)
+        {
+
+            switch (type)
+            {
+                case LogType.Assert:
+                case LogType.Warning:
+                    Logger.Warning($"{condition}{stackTrace}");
+                    break;
+                case LogType.Log:
+                    Logger.Warning($"{condition}{stackTrace}");
+                    break;
+                case LogType.Error:
+                case LogType.Exception:
+                    Logger.Warning($"{condition}{stackTrace}");
+                    break;
+            }
+        }
+
+        public void OnFixedUpdate()
+        {
+            
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.CapsLock))
+            {
+                PerfectLootChance = !PerfectLootChance;
+                Logger.Debug($"PerfectLootChance: {PerfectLootChance}");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                PerfectFuelCellChance = !PerfectFuelCellChance;
+                Logger.Debug($"PerfectFuelCellChance/ShellChance: {PerfectFuelCellChance}");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                PerfectProcItemChance = !PerfectProcItemChance;
+                Logger.Debug($"PerfectProcItemChance: {PerfectProcItemChance}");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                PerfectLegendaryChance = !PerfectLegendaryChance;
+                Logger.Debug($"PerfectLegendaryChance: {PerfectLegendaryChance}");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                InfiniteRecycling = !InfiniteRecycling;
+                Logger.Debug($"InfiniteRecycling: {InfiniteRecycling}");
+            }
+            if (Input.GetKeyDown(KeyCode.Numlock))
+            {
+                HighStacks = !HighStacks;
+                Logger.Debug($"HighStacks: {HighStacks}");
+            }
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                var sceneDirector = Resources.FindObjectsOfTypeAll<SceneDirector>().FirstOrDefault();
+                var directorCore = Resources.FindObjectsOfTypeAll<DirectorCore>().FirstOrDefault();
+
+                DirectorPlacementRule placementRule = new DirectorPlacementRule
+                {
+                    placementMode = DirectorPlacementRule.PlacementMode.Random
+                };
+                var categories = ClassicStageInfo.instance.interactableCategories;
+                var card = categories.categories.SelectMany(x => x.cards).First(x => x.spawnCard.name == "iscShrineBoss");
+
+                directorCore.TrySpawnObject(new DirectorSpawnRequest(card.spawnCard, placementRule, sceneDirector.GetField<Xoroshiro128Plus>("rng")));
+
+                Logger.Debug("Placed new Shrine of the Mountain");
+            }
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                var sceneDirector = Resources.FindObjectsOfTypeAll<SceneDirector>().FirstOrDefault();
+                var directorCore = Resources.FindObjectsOfTypeAll<DirectorCore>().FirstOrDefault();
+
+                DirectorPlacementRule placementRule = new DirectorPlacementRule
+                {
+                    placementMode = DirectorPlacementRule.PlacementMode.Random
+                };
+                var categories = ClassicStageInfo.instance.interactableCategories;
+                var card = categories.categories.SelectMany(x => x.cards).First(x => x.spawnCard.name == "iscShrineBoss");
+
+                for (var i = 0; i < 50; i++) directorCore.TrySpawnObject(new DirectorSpawnRequest(card.spawnCard, placementRule, sceneDirector.GetField<Xoroshiro128Plus>("rng")));
+
+                Logger.Debug("Placed new Shrine of the Mountain (50x)");
+            }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Logger.Info("Giving all players lunar coins");
+                var bodies = Resources.FindObjectsOfTypeAll<CharacterBody>().Where(x => x.isPlayerControlled);
+                foreach (var body in bodies) Util.LookUpBodyNetworkUser(body).AwardLunarCoins(2000000000);
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Logger.Info("Giving self lunar coins");
+                NetworkUser.readOnlyInstancesList.Where(x => x.hasAuthority).First().AwardLunarCoins(2000000000);
+            }
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                foreach (var achievement in AchievementManager.allAchievementDefs)
+                {
+                    foreach (LocalUser user in LocalUserManager.readOnlyLocalUsersList)
+                    {
+                        AchievementManager.GetUserAchievementManager(user).GrantAchievement(achievement);
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                var voteControllers = Resources.FindObjectsOfTypeAll<VoteController>();
+                foreach (var controller in voteControllers)
+                {
+                    controller.InvokeMethod("InitializeVoters");
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                SimpleDialogBox simpleDialogBox = SimpleDialogBox.Create(null);
+                simpleDialogBox.AddCancelButton(CommonLanguageTokens.ok, Array.Empty<object>());
+                simpleDialogBox.AddCommandButton("command!", CommonLanguageTokens.ok, Array.Empty<object>());
+                simpleDialogBox.headerLabel.text = "BAHAHA YOU PRESSED F";
+                simpleDialogBox.descriptionLabel.text = "BAHAHA YOU PRESSED F, AND THIS IS A DESCRIPTION";
+            }
+        }
+    }
+}
